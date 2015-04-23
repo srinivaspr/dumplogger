d162767d2561946579c7345d6d8737d8fe848abd
srinivaspr <srinivasapavankumarreddy@gmail.com>
2015-04-23 06:24:15 -0700

    dumplogger

---
+++ src/main/java/com/logger/dumplogger/DumpLog.java
@@ -0,0 +1,30 @@
+package com.logger.dumplogger;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DumpLog {
+	
+	private static Logger logger = LoggerFactory.getLogger(DumpLog.class);
+	
+	@PostConstruct
+	public void dumpLog()
+	{
+		int i=0;
+		while(true)
+		{
+			logger.info("Logging.................");
+			i++;
+			if(i==100000)
+			{
+				logger.info("testing");
+				i=0;
+			}
+		}
+	}
+
+}
